ce775d7ad69a07cb8a05ad120bf172beb3af0378
lucak <lucak@141.84.13.215>
2017-07-04 14:43:01 +0200

    hallo

--- J-L-mid-term-project/mid-term-project/com/lucakellerstrass/Test/Testklasse2.java
+++ J-L-mid-term-project/mid-term-project/com/lucakellerstrass/Test/Testklasse2.java
@@ -1,10 +1,9 @@
 package com.lucakellerstrass.Test;
 
-public class Testklasse {
+public class Testklasse2 {
 
 	public static void main(String[] args) {
-		System.out.println("Das ist cool");
-
+		// macht irgendwas
 	}
 
 }
